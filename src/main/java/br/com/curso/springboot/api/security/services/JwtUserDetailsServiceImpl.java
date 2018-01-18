388860e73178033683d385286755d8a67e5065bd
Vinícius Kreusch da Cunha <viniciuskrc1@gmail.com>
2018-01-17 22:38:52 -0200

    Configuracao da api de jwt para tokenizar o acessso.

---
+++ src/main/java/br/com/curso/springboot/api/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package br.com.curso.springboot.api.security.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.curso.springboot.api.entidades.Funcionario;
+import br.com.curso.springboot.api.security.JwtUserFactory;
+import br.com.curso.springboot.api.services.FuncionarioService;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
--- src/main/java/br/com/curso/springboot/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/br/com/curso/springboot/api/services/impl/LancamentoServiceImpl.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
